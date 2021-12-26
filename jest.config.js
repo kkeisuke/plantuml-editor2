1fa55b079120272b86819b74f7f8d2a1c1dfe5dc
kimura keisuke <mail@kkeisuke.com>
2021-12-27 01:43:57 +0900

    jest 追加

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/?(*.)spec.ts']
+}
