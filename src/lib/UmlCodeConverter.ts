e70f7cb9956384d45b6ffca63821e1814493714e
kimura keisuke <mail@kkeisuke.com>
2021-12-27 11:32:30 +0900

    UML を markdown の画像文字列に変換する処理

---
+++ src/lib/UmlCodeConverter.ts
@@ -0,0 +1,53 @@
+import { encode } from 'plantuml-encoder'
+
+const startuml = ['@startuml', '@startmindmap', '@startditaa', '@startgantt', '@startwbs']
+const enduml = ['@enduml', '@endmindmap', '@endditaa', '@endgantt', '@endwbs']
+
+export const parser = (code: string) => {
+  const lines = code.split('\n')
+  const separated = ['']
+
+  lines.forEach((line, i) => {
+    if (startuml.includes(line)) {
+      separated.push(line + '\n')
+    } else {
+      separated[separated.length - 1] += line
+      // 最終行には \n を追加しない
+      if (i !== lines.length - 1) {
+        if (enduml.includes(line)) {
+          separated.push('\n')
+        } else {
+          separated[separated.length - 1] += '\n'
+        }
+      }
+    }
+  })
+
+  return separated
+}
+
+export type ConvertOptions = {
+  server: string
+  ext?: string
+}
+
+export const convertToMd = (code: string, { server, ext = 'svg' }: ConvertOptions) => {
+  const parserd = parser(code)
+  const imgs: string[] = []
+  const markdowns = parserd.map((md) => {
+    const hasStart = startuml.find((start) => md.indexOf(start) === 0) !== undefined
+    const hasEnd = enduml.find((end) => new RegExp(`${end}$`).test(md)) !== undefined
+    if (hasStart && hasEnd) {
+      const encoded = encode(md)
+      const uml = `${server}/${ext}/${encoded}`
+      imgs.push(uml)
+      return `![](${uml})`
+    }
+    return md
+  })
+
+  return {
+    md: markdowns.join(''),
+    imgs
+  }
+}
---
+++ tests/lib/UmlCodeConverter.spec.ts
@@ -0,0 +1,76 @@
+import { convertToMd, parser } from '../../src/lib/UmlCodeConverter'
+
+describe('UmlCodeParser', () => {
+  describe('parser', () => {
+    it('UMLが一つの場合', () => {
+      const parsed = parser('# スタート\n\n@startuml\n\nテスト\n\n@enduml\n\n# エンド')
+      expect(parsed.length).toBe(3)
+      expect(parsed[0]).toBe('# スタート\n\n')
+      expect(parsed[1]).toBe('@startuml\n\nテスト\n\n@enduml')
+      expect(parsed[2]).toBe('\n\n# エンド')
+    })
+    it('UMLが複数の場合', () => {
+      const parsed = parser('# スタート\n\n@startuml\n\nテスト\n\n@enduml\n\n# エンド\n\n@startuml\n\nテスト\n\n@enduml')
+      expect(parsed.length).toBe(4)
+      expect(parsed[0]).toBe('# スタート\n\n')
+      expect(parsed[1]).toBe('@startuml\n\nテスト\n\n@enduml')
+      expect(parsed[2]).toBe('\n\n# エンド\n\n')
+      expect(parsed[3]).toBe('@startuml\n\nテスト\n\n@enduml')
+    })
+    it('UMLが連続する場合', () => {
+      const parsed = parser('# スタート\n\n@startuml\n\nテスト\n\n@enduml\n\n\n\n@startuml\n\nテスト\n\n@enduml')
+      expect(parsed.length).toBe(4)
+      expect(parsed[0]).toBe('# スタート\n\n')
+      expect(parsed[1]).toBe('@startuml\n\nテスト\n\n@enduml')
+      expect(parsed[2]).toBe('\n\n\n\n')
+      expect(parsed[3]).toBe('@startuml\n\nテスト\n\n@enduml')
+    })
+    it('@startuml がない場合', () => {
+      const parsed = parser('# スタート\n\nテスト\n\n@enduml\n\n# エンド')
+      expect(parsed.length).toBe(2)
+      expect(parsed[0]).toBe('# スタート\n\nテスト\n\n@enduml')
+      expect(parsed[1]).toBe('\n\n# エンド')
+    })
+    it('@enduml がない場合', () => {
+      const parsed = parser('# スタート\n\n@startuml\n\nテスト\n\n# エンド')
+      expect(parsed.length).toBe(2)
+      expect(parsed[0]).toBe('# スタート\n\n')
+      expect(parsed[1]).toBe('@startuml\n\nテスト\n\n# エンド')
+    })
+  })
+  describe('convertToMd', () => {
+    const uml = '@startuml\n\nobject テスト\n\n@enduml'
+    const img = 'http://localhost/svg/SoWkIImgAStDuUBAJyfAJIvHUDpSzhXfv-Fc3YukXzIy5A0K0000'
+    const encoded = `![](${img})`
+
+    it('UMLが一つの場合', () => {
+      const code = `# スタート\n\n${uml}\n\n# エンド`
+      const { md, imgs } = convertToMd(code, { server: 'http://localhost' })
+      expect(md).toBe(`# スタート\n\n${encoded}\n\n# エンド`)
+      expect(imgs.length).toBe(1)
+      expect(imgs[0]).toBe(img)
+    })
+    it('UMLが2つの場合', () => {
+      const code = `# スタート\n\n${uml}\n\n# エンド\n\n${uml}`
+      const { md, imgs } = convertToMd(code, { server: 'http://localhost' })
+      expect(md).toBe(`# スタート\n\n${encoded}\n\n# エンド\n\n${encoded}`)
+      expect(imgs.length).toBe(2)
+      expect(imgs[0]).toBe(img)
+      expect(imgs[1]).toBe(img)
+    })
+    it('@startuml がない場合', () => {
+      const code = '# スタート\n\nテスト\n\n@enduml\n\n# エンド'
+      const { md, imgs } = convertToMd(code, { server: 'http://localhost' })
+      expect(md).toBe(code)
+      expect(imgs.length).toBe(0)
+      expect(imgs[0]).toBe(undefined)
+    })
+    it('@enduml がない場合', () => {
+      const code = '# スタート\n\n@startuml\n\nテスト\n\n# エンド'
+      const { md, imgs } = convertToMd(code, { server: 'http://localhost' })
+      expect(md).toBe(code)
+      expect(imgs.length).toBe(0)
+      expect(imgs[0]).toBe(undefined)
+    })
+  })
+})
