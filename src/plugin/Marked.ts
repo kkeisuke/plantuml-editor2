216894c93411c68757e6cbae46083e97f0d403c6
kimura keisuke <mail@kkeisuke.com>
2021-12-28 14:56:35 +0900

    marked & dompurify plugin 追加

---
+++ src/plugin/Marked.ts
@@ -0,0 +1,14 @@
+import { marked } from 'marked'
+import DOMPurify from 'dompurify'
+
+// https://github.com/cure53/DOMPurify/blob/master/demos/hooks-target-blank-demo.html
+DOMPurify.addHook('afterSanitizeAttributes', (node) => {
+  if ('target' in node) {
+    node.setAttribute('target', '_blank')
+    node.setAttribute('rel', 'noopener noreferrer')
+  }
+})
+
+export const convertToHtml = (md: string) => {
+  return DOMPurify.sanitize(marked(md))
+}
