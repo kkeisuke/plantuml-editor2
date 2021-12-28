276389ed5a8c6e20066e1ef0d105587d8da8f61a
kimura keisuke <mail@kkeisuke.com>
2021-12-28 16:12:48 +0900

    store 追加

---
+++ src/store/UseUmlCodeCollection.ts
@@ -0,0 +1,49 @@
+import { computed, inject, InjectionKey, provide, reactive } from 'vue'
+import { umlCodeCollectionRepository } from '@/repository/UmlCodeCollectionRepository'
+import { getDefaultUmlCode, UmlCode } from '@/entities/UmlCode'
+
+type State = {
+  umlCodes: UmlCode[]
+}
+
+const useUmlCodeCollection = () => {
+  const state = reactive<State>({
+    umlCodes: []
+  })
+
+  const fetch = async () => {
+    state.umlCodes = await umlCodeCollectionRepository.fetch()
+  }
+
+  const add = async (code: UmlCode['code']) => {
+    const id = await umlCodeCollectionRepository.add({ ...getDefaultUmlCode(), code })
+    fetch()
+    return id
+  }
+
+  return {
+    umlCodes: computed(() => state.umlCodes),
+    fetch,
+    add
+  }
+}
+
+const USE_UML_CODE_COLLECTION: InjectionKey<ReturnType<typeof useUmlCodeCollection>> = Symbol('USE_UML_CODE_COLLECTION')
+
+export const provideUseUmlCodeCollection = () => {
+  const useObj = useUmlCodeCollection()
+
+  provide(USE_UML_CODE_COLLECTION, useObj)
+
+  return useObj
+}
+
+export const injectUseUmlCodeCollection = () => {
+  const useObj = inject(USE_UML_CODE_COLLECTION)
+
+  if (useObj) {
+    return useObj
+  } else {
+    throw new Error('error injectUseUmlCodeCollection')
+  }
+}
---
+++ src/store/UseUmlCodeSingle.ts
@@ -0,0 +1,89 @@
+import { computed, inject, InjectionKey, provide, reactive, ref, toRaw } from 'vue'
+import { umlCodeCacheRepository } from '@/repository/UmlCodeCacheRepository'
+import { umlCodeSingleRepository } from '@/repository/UmlCodeSingleRepository'
+import { getDefaultUmlCode, UmlCode } from '@/entities/UmlCode'
+import { convertToMd } from '@/lib/UmlCodeConverter'
+import { convertToHtml } from '@/plugin/marked'
+
+const useUmlCodeSingle = () => {
+  const state = reactive({
+    umlCode: getDefaultUmlCode()
+  })
+  const htmlString = ref('')
+
+  const read = async (id: UmlCode['id']) => {
+    const result = await umlCodeSingleRepository.read(id)
+    Object.assign(state.umlCode, result)
+    umlCodeCacheRepository.save(id)
+  }
+
+  const readCache = async () => {
+    const id = umlCodeCacheRepository.fetch()
+    id && (await read(id))
+  }
+
+  const getCode = () => {
+    return state.umlCode.code
+  }
+
+  const update = async (code: UmlCode['code']) => {
+    if (!state.umlCode.id) {
+      return
+    }
+    state.umlCode.code = code
+    const result = await umlCodeSingleRepository.update(toRaw(state.umlCode))
+    // updatedAt が更新される
+    Object.assign(state.umlCode, result)
+  }
+
+  const destroy = async (umlCode: UmlCode) => {
+    await umlCodeSingleRepository.delete(umlCode.id)
+    if (state.umlCode.id === umlCode.id) {
+      Object.assign(state.umlCode, getDefaultUmlCode())
+      htmlString.value = ''
+      umlCodeCacheRepository.reset()
+    }
+  }
+
+  const renderHtml = async (code: UmlCode['code']) => {
+    if (!state.umlCode.id) {
+      return
+    }
+    const { md, imgs } = convertToMd(code, { server: import.meta.env.VITE_PLANTUML_SERVER })
+    state.umlCode.imgs = imgs
+    // imgs を更新するため
+    await update(code)
+    htmlString.value = convertToHtml(md)
+  }
+
+  return {
+    umlCode: computed(() => state.umlCode),
+    htmlString: computed(() => htmlString.value),
+    read,
+    readCache,
+    getCode,
+    update,
+    destroy,
+    renderHtml
+  }
+}
+
+const USE_UML_CODE_SINGLE: InjectionKey<ReturnType<typeof useUmlCodeSingle>> = Symbol('USE_UML_CODE_SINGLE')
+
+export const provideUseUmlCodeSingle = () => {
+  const useObj = useUmlCodeSingle()
+
+  provide(USE_UML_CODE_SINGLE, useObj)
+
+  return useObj
+}
+
+export const injectUseUmlCodeSingle = () => {
+  const useObj = inject(USE_UML_CODE_SINGLE)
+
+  if (useObj) {
+    return useObj
+  } else {
+    throw new Error('error injectUseUmlCodeSingle')
+  }
+}
