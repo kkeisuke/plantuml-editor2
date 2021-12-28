5e118c46aadc36598a4f6cc45db1e458524032a0
kimura keisuke <mail@kkeisuke.com>
2021-12-28 12:18:55 +0900

    DateTimeFormat 関連を集約する lib 追加

---
+++ src/lib/DateTimeFormat.ts
@@ -0,0 +1,11 @@
+export const getNow = () => {
+  return new Intl.DateTimeFormat('default', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+  }).format(new Date())
+}
