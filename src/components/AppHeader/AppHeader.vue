7d66eb5db5de037d7e54acad278137b36a940ab9
kimura keisuke <mail@kkeisuke.com>
2021-12-29 16:08:17 +0900

    AppHeader に header タグも含めるように修正

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -5,16 +5,18 @@ import CheatSheetSwitch from '@/components/AppHeader/CheatSheetSwitch.vue'
 </script>
 
 <template>
-  <nav class="navbar navbar-dark bg-dark">
-    <div class="container-fluid justify-content-start">
-      <side-menu-btn class="me-3" />
-      <span class="navbar-brand me-5 mb-0 h1 text-white-50">PlantUML Editor</span>
-      <ul class="navbar-nav flex-row align-items-center gap-3 me-4">
-        <template-menu />
-      </ul>
-      <cheat-sheet-switch />
-    </div>
-  </nav>
+  <header>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid justify-content-start">
+        <side-menu-btn class="me-3" />
+        <span class="navbar-brand me-5 mb-0 h1 text-white-50">PlantUML Editor</span>
+        <ul class="navbar-nav flex-row align-items-center gap-3 me-4">
+          <template-menu />
+        </ul>
+        <cheat-sheet-switch />
+      </div>
+    </nav>
+  </header>
 </template>
 
 <style scoped></style>
