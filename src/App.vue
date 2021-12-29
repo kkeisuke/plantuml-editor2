d3b3b3061e3f80d4a3e1b1276b6bd4b0f80e49a3
kimura keisuke <mail@kkeisuke.com>
2021-12-29 16:11:09 +0900

    App に PlantumlEditor 追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PlantumlEditor from '@/components/PlantumlEditor.vue'
+</script>
 
-<template></template>
+<template>
+  <plantuml-editor />
+</template>
 
-<style></style>
+<style>
+html {
+  font-size: 0.875rem;
+}
+[role='button'] {
+  user-select: none;
+}
+</style>
