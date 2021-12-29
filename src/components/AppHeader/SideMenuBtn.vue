86788429c2bcc8750a6a554233eeedaa09a4ecb0
kimura keisuke <mail@kkeisuke.com>
2021-12-29 15:39:38 +0900

    SideMenuBtn 追加

---
+++ src/components/AppHeader/SideMenuBtn.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import chevronLeft from '@/assets/icons/bi-chevron-left.svg?url'
+import chevronRight from '@/assets/icons/bi-chevron-right.svg?url'
+import { injectUseLayout } from '@/store/UseLayout'
+
+const { isShowMenu, showMenu, hideMenu } = injectUseLayout()
+
+const onClick = () => {
+  isShowMenu.value ? hideMenu() : showMenu()
+}
+</script>
+
+<template>
+  <svg role="button" class="SideMenuBtn text-secondary" @click="onClick">
+    <use :href="`${isShowMenu ? chevronLeft : chevronRight}#icon`" />
+  </svg>
+</template>
+
+<style scoped>
+.SideMenuBtn {
+  width: 1.4rem;
+  height: 1.4rem;
+}
+</style>
