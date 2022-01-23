e91ac4f7c625be53eb35f7861dccdf15432200e5
kimura keisuke <mail@kkeisuke.com>
2022-01-23 14:44:12 +0900

    vscode plugin を使うと、.vue shim が不要になるらしい
    https://github.com/vuejs/create-vue/pull/26

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 interface ImportMetaEnv {
   readonly VITE_PLANTUML_SERVER: string
 }
