35bc6a676b66c63bad5bf9531f9b48d585bcba04
kimura keisuke <mail@kkeisuke.com>
2022-01-27 16:54:03 +0900

    jest 削除

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/tests'],
-  testMatch: ['**/?(*.)spec.ts']
-}
